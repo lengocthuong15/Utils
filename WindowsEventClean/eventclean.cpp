f338a6370283b3a023236db0d60f85f0690cba19
Thuong Le <lengocthuong15@gmail.com>
2019-12-07 16:40:37 +0700

    Combine convertToAscii and clean log at the same time

--- WindowsEventClean/eventclean.cpp
+++ WindowsEventClean/eventclean.cpp
@@ -4,12 +4,31 @@
 #include <regex> 
 #include <iterator> 
 
+
+void cleanEvent(const std::string& inputStr, std::string& outputStr)
+{
+	std::string result;
+	std::string tempStr;
+	std::regex regEx("<Event xmln.*/Keywords>");
+	std::regex_replace(back_inserter(tempStr), inputStr.begin(), inputStr.end(), regEx, "");
+
+	regEx = "<EventRecordID>.*<Data>";
+	std::regex_replace(back_inserter(result), tempStr.begin(), tempStr.end(), regEx, "");
+
+	tempStr = result;
+	result = "";
+	regEx = "TimeCreated SystemTime=";
+	std::regex_replace(back_inserter(result), tempStr.begin(), tempStr.end(), regEx, "");
+
+	outputStr = result;
+}
+
 int convertToASCII(const std::string& inputPath, const std::string outPath)
 {
 	std::ifstream inFile(inputPath);
 
 	if (inFile.is_open()) {
-		std::ofstream outFile(outPath, std::ofstream::binary);
+		std::ofstream outFile(outPath);
 		if (!outFile.is_open())
 		{
 			std::cout << "Cannot open output file";
@@ -31,9 +50,13 @@ int convertToASCII(const std::string& inputPath, const std::string outPath)
 					*s++ = buff;
 				}
 			}
-			buffer[counter] = 10;
-			outFile.write(buffer, counter + 1);
+
+			//Clean log
+			std::string tempStr = buffer;
 			delete[] buffer;
+			std::string outputStr;
+			cleanEvent(tempStr, outputStr);
+			outFile << outputStr << "\n";
 		}
 
 		inFile.close();
@@ -47,47 +70,37 @@ int convertToASCII(const std::string& inputPath, const std::string outPath)
 	return 0;
 }
 
-int cleanEvent(const std::string& inputPath, const std::string outPath)
-{
-	std::ifstream inFile(inputPath);
-
-	if (inFile.is_open()) {
-		std::ofstream outFile(outPath);
-		if (!outFile.is_open())
-		{
-			std::cout << "Cannot open output file";
-			return 1;
-		}
-		std::string line;
-		std::cout << "Processing......" << std::endl;
-		while (getline(inFile, line))
-		{
-			std::string result;
-			std::string tempStr;
-			std::regex regEx("<Event xmln.*/Keywords>");
-			std::regex_replace(back_inserter(tempStr), line.begin(), line.end(), regEx, "");
-
-			regEx = "<EventRecordID>.*<Data>";
-			std::regex_replace(back_inserter(result), tempStr.begin(), tempStr.end(), regEx, "");
-
-			tempStr = result;
-			result = "";
-			regEx = "TimeCreated SystemTime=";
-			std::regex_replace(back_inserter(result), tempStr.begin(), tempStr.end(), regEx, "");
-
-			outFile << result << "\n";
-		}
-
-		inFile.close();
-		outFile.close();
-	}
-	else
-	{
-		std::cout << "Cannot open input file";
-		return 1;
-	}
-	return 0;
-}
+//int cleanEvent(const std::string& inputPath, const std::string outPath)
+//{
+//	std::ifstream inFile(inputPath);
+//
+//	if (inFile.is_open()) {
+//		std::ofstream outFile(outPath);
+//		if (!outFile.is_open())
+//		{
+//			std::cout << "Cannot open output file";
+//			return 1;
+//		}
+//		std::string line;
+//		std::cout << "Processing......" << std::endl;
+//		while (getline(inFile, line))
+//		{
+//			std::string result;
+//			cleanEvent(line, result);
+//
+//			outFile << result << "\n";
+//		}
+//
+//		inFile.close();
+//		outFile.close();
+//	}
+//	else
+//	{
+//		std::cout << "Cannot open input file";
+//		return 1;
+//	}
+//	return 0;
+//}
 
 int main(int argc, char* argv[]) {
 
@@ -104,7 +117,7 @@ int main(int argc, char* argv[]) {
 
 	//bool isConvertOK = convertToASCII(argv[1], argv[2]);
 
-	bool isCleanOK = cleanEvent(argv[1], argv[2]);
+	bool isCleanOK = convertToASCII(argv[1], argv[2]);
 		
 }
 
